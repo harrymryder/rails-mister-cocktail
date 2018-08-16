57a79936447112459ab3b264ad924888e86927b2
Harry Ryder <harrymryder@gmail.com>
2018-08-16 12:12:55 +0100

    Models complete with validations

--- app/models/dose.rb
+++ app/models/dose.rb
@@ -1,4 +1,10 @@
 class Dose < ApplicationRecord
   belongs_to :cocktail
   belongs_to :ingredient
+
+  validates :description, presence: true
+  validates_associated :ingredient
+  validates_associated :cocktail
+  validates :ingredient, uniqueness: { scope: :cocktail }
+  validates :cocktail, uniqueness: { scope: :ingredient }
 end
